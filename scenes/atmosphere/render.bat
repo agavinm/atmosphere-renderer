68639a039a3b38f1cf9af2b0cb1050618c2b79c0
Andrés <agavinm@gmail.com>
2020-10-27 16:37:08 +0100

    Renders

---
+++ scenes/atmosphere/render.bat
@@ -0,0 +1,32 @@
+mitsuba -m scalar_spectral render7.xml -o render7_scalar_spectral.exr
+mitsuba -m scalar_spectral render8.xml -o render8_scalar_spectral.exr
+mitsuba -m scalar_spectral render9.xml -o render9_scalar_spectral.exr
+
+mitsuba -m scalar_spectral render7A.xml -o render7A_scalar_spectral.exr
+mitsuba -m scalar_spectral render8A.xml -o render8A_scalar_spectral.exr
+mitsuba -m scalar_spectral render9A.xml -o render9A_scalar_spectral.exr
+
+mitsuba -m scalar_spectral render7B.xml -o render7B_scalar_spectral.exr
+mitsuba -m scalar_spectral render8B.xml -o render8B_scalar_spectral.exr
+mitsuba -m scalar_spectral render9B.xml -o render9B_scalar_spectral.exr
+
+mitsuba -m scalar_spectral render7C.xml -o render7C_scalar_spectral.exr
+mitsuba -m scalar_spectral render8C.xml -o render8C_scalar_spectral.exr
+mitsuba -m scalar_spectral render9C.xml -o render9C_scalar_spectral.exr
+
+
+mitsuba -m packet_spectral render7.xml -o render7_packet_spectral.exr
+mitsuba -m packet_spectral render8.xml -o render8_packet_spectral.exr
+mitsuba -m packet_spectral render9.xml -o render9_packet_spectral.exr
+
+mitsuba -m packet_spectral render7A.xml -o render7A_packet_spectral.exr
+mitsuba -m packet_spectral render8A.xml -o render8A_packet_spectral.exr
+mitsuba -m packet_spectral render9A.xml -o render9A_packet_spectral.exr
+
+mitsuba -m packet_spectral render7B.xml -o render7B_packet_spectral.exr
+mitsuba -m packet_spectral render8B.xml -o render8B_packet_spectral.exr
+mitsuba -m packet_spectral render9B.xml -o render9B_packet_spectral.exr
+
+mitsuba -m packet_spectral render7C.xml -o render7C_packet_spectral.exr
+mitsuba -m packet_spectral render8C.xml -o render8C_packet_spectral.exr
+mitsuba -m packet_spectral render9C.xml -o render9C_packet_spectral.exr
---
+++ scenes/atmosphere/render.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+mitsuba -m scalar_spectral render1.xml -o render1_scalar_spectral.exr
+mitsuba -m scalar_spectral render2.xml -o render2_scalar_spectral.exr
+mitsuba -m scalar_spectral render3.xml -o render3_scalar_spectral.exr
+mitsuba -m scalar_spectral render4.xml -o render4_scalar_spectral.exr
+mitsuba -m scalar_spectral render5.xml -o render5_scalar_spectral.exr
+mitsuba -m scalar_spectral render6.xml -o render6_scalar_spectral.exr
+
+mitsuba -m scalar_spectral render1A.xml -o render1A_scalar_spectral.exr
+mitsuba -m scalar_spectral render2A.xml -o render2A_scalar_spectral.exr
+mitsuba -m scalar_spectral render3A.xml -o render3A_scalar_spectral.exr
+mitsuba -m scalar_spectral render4A.xml -o render4A_scalar_spectral.exr
+mitsuba -m scalar_spectral render5A.xml -o render5A_scalar_spectral.exr
+mitsuba -m scalar_spectral render6A.xml -o render6A_scalar_spectral.exr
+
+mitsuba -m scalar_spectral render1B.xml -o render1B_scalar_spectral.exr
+mitsuba -m scalar_spectral render2B.xml -o render2B_scalar_spectral.exr
+mitsuba -m scalar_spectral render3B.xml -o render3B_scalar_spectral.exr
+mitsuba -m scalar_spectral render4B.xml -o render4B_scalar_spectral.exr
+mitsuba -m scalar_spectral render5B.xml -o render5B_scalar_spectral.exr
+mitsuba -m scalar_spectral render6B.xml -o render6B_scalar_spectral.exr
+
+mitsuba -m scalar_spectral render1C.xml -o render1C_scalar_spectral.exr
+mitsuba -m scalar_spectral render2C.xml -o render2C_scalar_spectral.exr
+mitsuba -m scalar_spectral render3C.xml -o render3C_scalar_spectral.exr
+mitsuba -m scalar_spectral render4C.xml -o render4C_scalar_spectral.exr
+mitsuba -m scalar_spectral render5C.xml -o render5C_scalar_spectral.exr
+mitsuba -m scalar_spectral render6C.xml -o render6C_scalar_spectral.exr
+
+
+mitsuba -m packet_spectral render1.xml -o render1_packet_spectral.exr
+mitsuba -m packet_spectral render2.xml -o render2_packet_spectral.exr
+mitsuba -m packet_spectral render3.xml -o render3_packet_spectral.exr
+mitsuba -m packet_spectral render4.xml -o render4_packet_spectral.exr
+mitsuba -m packet_spectral render5.xml -o render5_packet_spectral.exr
+mitsuba -m packet_spectral render6.xml -o render6_packet_spectral.exr
+
+mitsuba -m packet_spectral render1A.xml -o render1A_packet_spectral.exr
+mitsuba -m packet_spectral render2A.xml -o render2A_packet_spectral.exr
+mitsuba -m packet_spectral render3A.xml -o render3A_packet_spectral.exr
+mitsuba -m packet_spectral render4A.xml -o render4A_packet_spectral.exr
+mitsuba -m packet_spectral render5A.xml -o render5A_packet_spectral.exr
+mitsuba -m packet_spectral render6A.xml -o render6A_packet_spectral.exr
+
+mitsuba -m packet_spectral render1B.xml -o render1B_packet_spectral.exr
+mitsuba -m packet_spectral render2B.xml -o render2B_packet_spectral.exr
+mitsuba -m packet_spectral render3B.xml -o render3B_packet_spectral.exr
+mitsuba -m packet_spectral render4B.xml -o render4B_packet_spectral.exr
+mitsuba -m packet_spectral render5B.xml -o render5B_packet_spectral.exr
+mitsuba -m packet_spectral render6B.xml -o render6B_packet_spectral.exr
+
+mitsuba -m packet_spectral render1C.xml -o render1C_packet_spectral.exr
+mitsuba -m packet_spectral render2C.xml -o render2C_packet_spectral.exr
+mitsuba -m packet_spectral render3C.xml -o render3C_packet_spectral.exr
+mitsuba -m packet_spectral render4C.xml -o render4C_packet_spectral.exr
+mitsuba -m packet_spectral render5C.xml -o render5C_packet_spectral.exr
+mitsuba -m packet_spectral render6C.xml -o render6C_packet_spectral.exr
